6d64578b0daed013a6e5e96e93056e47f59bf6f8
muhammad-waseem-netixsol <muhammad_waseem_intern@netixsol.com>
2024-02-09 15:23:18 +0500

    CODE

---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  // swagger setup
+  const config = new DocumentBuilder()
+  .setTitle('Contacts Api')
+  .setDescription('Built in NEST.js for contact CRUD operations...')
+  .setVersion('1.0')
+  .addTag('contacts')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document, {
+    customSiteTitle: 'Backend Generator',
+    customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+    ],
+  });
+SwaggerModule.setup('api', app, document);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3001);
+}
+bootstrap();
